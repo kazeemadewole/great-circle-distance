e62987b7ac4a7790922d1e616f5f65d03b7adc09
whocares0 <kazeem.adewole@decagon.dev>
2021-06-20 19:20:14 +0100

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CircleDistanceController } from './circle-distance/circle-distance.controller';
-import { CircleDistanceService } from './circle-distance/circle-distance.service';
 import { CircleDistanceModule } from './circle-distance/circle-distance.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from './config/constants';
